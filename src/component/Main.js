b780ea3b96ec522cca16c77a8ff2cffbcbed8240
lmNotTryHard <97128146+lmNotTryHard@users.noreply.github.com>
2023-12-28 20:25:51 +0600

    update

--- src/component/Main.js
+++ src/component/Main.js
@@ -1,46 +1,89 @@
-import React from 'react';
-import './Main.css'
+import React, { useRef } from 'react';
+import copy from 'clipboard-copy';
+import './Main.css';
 
 // components
 import Header from './Header.js';
 import News from './News.js';
 
+// assets
+import logo from '../logo.svg'
 
+const CopyTextComponent = ({ text }) => {
+    const textRef = useRef(null);
 
+    const handleTextClick = () => {
+        if (textRef.current) {
+            copy(textRef.current.textContent);
+            alert('Текст скопирован!');
+        }
+    };
+
+    return (
+        <p ref={textRef} onClick={handleTextClick} style={{ cursor: 'pointer' }}>
+            {text}
+        </p>
+    );
+};
 
 export default function Main() {
     return (
         <div className='main'>
-            <div className='section-1'><Header /></div>
-            <div className='section-2'><News /></div>
-
-
+            <div className='section-1'>
+                <Header />
+            </div>
+            <div className='section-2'>
+                <News />
+            </div>
 
             <div className='section-3'>
                 <div className='footer'>
                     <div className='section'>
                         <div className='logo'>
-                            <img src='#' />
-                            КАЗАХСТАНСКАЯ АССОЦИАЦИЯ БПЛА
+                            <img src={logo} alt='Logo' width='135px' />
+                            <div className='titles'>
+                                <span className='post-title'>
+                                    ОБЪЕДИНЕНИЕ ЮРИДИЧЕСКИХ ЛИЦ И ИНДИВИДУАЛЬНЫХ ПРЕДПРИНИМАТЕЛЕЙ
+                                </span>
+                                <span className='name'>
+                                    КАЗАХСТАНСКАЯ АССОЦИАЦИЯ БПЛА
+                                </span>
+                            </div>
                         </div>
-                        <div className='navbar'>
-                            
+                        <div className='list'>
+                            <ul>
+                                <li><span class="material-symbols-outlined">phone_in_talk</span><CopyTextComponent text="+00000000000" /></li>
+                                <li>
+                                    <a href='#'><span class="material-symbols-outlined">mail</span>example@gmail.com</a>
+                                </li>
+                                <li>
+                                    <span class="material-symbols-outlined">location_on</span>Республика Казахстана, г. Астана
+                                </li>
+                            </ul>
                         </div>
                     </div>
 
-
-                    <div className='navbar'>
-                        <ul>
-                            <li></li>
-                            <li><a href='#'></a></li>
-                            <li><a href='#'></a></li>
-                            <li><a href='#'></a></li>
-                            <li><a href='#'></a></li>
-                        </ul>
+                    <div className='section'>
+                        <div className='list'>
+                            <ul>
+                                <li></li>
+                                <li>
+                                    <a href='#'></a>
+                                </li>
+                                <li>
+                                    <a href='#'></a>
+                                </li>
+                                <li>
+                                    <a href='#'></a>
+                                </li>
+                                <li>
+                                    <a href='#'></a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
-
 
 
             <div className='section-4'></div>
