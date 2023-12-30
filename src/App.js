4e7830fd38588b2c1ac7faf80869b364525ffc37
lmNotTryHard <97128146+lmNotTryHard@users.noreply.github.com>
2023-12-30 15:51:35 +0600

    prelaoder

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import { TailSpin } from 'react-loader-spinner';
 
-// component
-import Main from './component/Main';
-import Navbar from './component/Navbar';
+// Lazy-loaded components
+const LazyMain = lazy(() => import('./component/Main'));
+const LazyNavbar = lazy(() => import('./component/Navbar'));
 
 function App() {
   return (
     <BrowserRouter>
-      <div className="App">
-        <Navbar />
-
-        <Routes>
-          <Route path='/' element={<Main />} />
-        </Routes>
+      <div className={`App fade-in`}>
+        <Suspense
+          fallback={
+            <div className="loader-container">
+              <TailSpin
+                color="#1E5691"
+                height={80}
+                width={80}
+                timeout={3000}
+              />
+            </div>
+          }
+        >
+          <LazyNavbar />
+          <Routes>
+            <Route path="/" element={<LazyMain />} />
+          </Routes>
+        </Suspense>
       </div>
     </BrowserRouter>
   );
